b28fce3d51ca5dcf8916b5efce603c9de04ff2b1
rehan aditya permana <rehanadityapermana@hotmail.com>
2026-09-22 23:53:01 +0700

    fix view binding and update gitignore

--- api/index.php
+++ api/index.php
@@ -1,22 +1,22 @@
 <?php
 
-// Arahkan storage dan cache ke folder /tmp bawaan Vercel
 $storagePath = '/tmp/storage';
-if (!is_dir($storagePath)) {
-    mkdir($storagePath . '/framework/views', 0755, true);
+$viewsPath = '/tmp/storage/framework/views';
+
+if (!is_dir($viewsPath)) {
     mkdir($storagePath . '/framework/sessions', 0755, true);
+    mkdir($storagePath . '/framework/views', 0755, true);
     mkdir($storagePath . '/framework/cache', 0755, true);
     mkdir($storagePath . '/logs', 0755, true);
 }
 
-// Load Autoload Composer
 require __DIR__ . '/../vendor/autoload.php';
 
-// Inisialisasi App Laravel
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Override path storage ke /tmp
+// Set path storage & view compiled ke /tmp
 $app->useStoragePath($storagePath);
+$app['config']->set('view.compiled', $viewsPath);
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
