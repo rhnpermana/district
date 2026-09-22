37f687bd0081aae52b7322026111b0bfa1a089a4
rehan aditya permana <rehanadityapermana@hotmail.com>
2026-09-23 01:03:22 +0700

    fix error part-6

--- api/index.php
+++ api/index.php
@@ -1,29 +1,3 @@
 <?php
 
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// Konfigurasi otomatis untuk Vercel Read-Only Filesystem
-if (isset($_ENV['VERCEL_REGION']) || isset($_SERVER['VERCEL_REGION'])) {
-    $storagePath = '/tmp/storage';
-    if (!is_dir($storagePath . '/framework/views')) {
-        mkdir($storagePath . '/framework/sessions', 0755, true);
-        mkdir($storagePath . '/framework/views', 0755, true);
-        mkdir($storagePath . '/framework/cache', 0755, true);
-        mkdir($storagePath . '/logs', 0755, true);
-    }
-    putenv("VIEW_COMPILED_PATH={$storagePath}/framework/views");
-}
-
-// Check If The Application Is Under Maintenance...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register The Auto Loader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+require __DIR__ . '/../public/index.php';
--- public/index.php
+++ public/index.php
@@ -5,6 +5,19 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Konfigurasi otomatis direktori temporary di Vercel (Read-Only Fix)
+if (isset($_ENV['VERCEL_REGION']) || isset($_SERVER['VERCEL_REGION'])) {
+    $storagePath = '/tmp/storage';
+    if (!is_dir($storagePath . '/framework/views')) {
+        mkdir($storagePath . '/framework/sessions', 0755, true);
+        mkdir($storagePath . '/framework/views', 0755, true);
+        mkdir($storagePath . '/framework/cache', 0755, true);
+        mkdir($storagePath . '/logs', 0755, true);
+    }
+    putenv("VIEW_COMPILED_PATH={$storagePath}/framework/views");
+    $_ENV['VIEW_COMPILED_PATH'] = "{$storagePath}/framework/views";
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
