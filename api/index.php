0c1e14c423fb5def8ad3a9da66e981a0d262215a
rehan aditya permana <rehanadityapermana@hotmail.com>
2026-09-22 23:15:57 +0700

    fix api index.php and vercel json config

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Load autoload file dari Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Jalankan aplikasi Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
