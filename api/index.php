fa7e759b94d87351a673296a8987374af6257689
rehan aditya permana <rehanadityapermana@hotmail.com>
2026-09-22 23:44:35 +0700

    fix view compiled path for vercel serverless

--- api/index.php
+++ api/index.php
@@ -1,10 +1,22 @@
 <?php
 
-// Load autoload file dari Composer
+// Arahkan storage dan cache ke folder /tmp bawaan Vercel
+$storagePath = '/tmp/storage';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath . '/framework/views', 0755, true);
+    mkdir($storagePath . '/framework/sessions', 0755, true);
+    mkdir($storagePath . '/framework/cache', 0755, true);
+    mkdir($storagePath . '/logs', 0755, true);
+}
+
+// Load Autoload Composer
 require __DIR__ . '/../vendor/autoload.php';
 
-// Jalankan aplikasi Laravel
+// Inisialisasi App Laravel
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Override path storage ke /tmp
+$app->useStoragePath($storagePath);
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
